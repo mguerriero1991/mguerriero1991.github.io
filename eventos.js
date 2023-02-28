89a68cf485b8c1fcf1dfa0435a5efc86edfae8c3
mguerriero1991 <126627506+mguerriero1991@users.noreply.github.com>
2023-02-28 20:46:17 -0300

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,11 @@
+document.getElementById("fondo_n").addEventListener("click", function(){
+    console.log ("Se activó el modo de lectura nocturna");
+    document.body.style.backgroundColor = "grey";
+});
+document.getElementById("fondo_d").addEventListener("click", function(){
+    console.log ("Se activó el modo de lectura diurna");
+    document.body.style.backgroundColor = "white";
+});
+document.getElementById("bot_subm").addEventListener("click", function(){
+    console.log ("Mensaje enviado el dia: "+Date());
+});
