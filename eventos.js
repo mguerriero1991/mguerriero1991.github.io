b3d57f0ada1018f4f8c9f83fc32915f30e083f06
mguerriero1991 <126627506+mguerriero1991@users.noreply.github.com>
2023-03-23 02:03:24 -0300

    Add files via upload
    Modificacion de la interfaz completa (posterior a la 1er entrega)

--- eventos.js
+++ eventos.js
@@ -1,13 +1,6 @@
-document.getElementById("fondo_n").addEventListener("click", function(){
-    console.log ("Se activó el modo de lectura nocturna");
-    document.body.style.backgroundColor = "grey";
-});
-document.getElementById("fondo_d").addEventListener("click", function(){
-    console.log ("Se activó el modo de lectura diurna");
-    document.body.style.backgroundColor = "white";
-});
 document.getElementById("bot_subm").addEventListener("click", function(){
     if (document.getElementById("nombre") !== "" && document.getElementById("mail").value !== ""){
         localStorage.horario = "Mensaje enviado el dia: "+Date();
+        //console.log ("Mensaje enviado el dia: "+Date());
     }
 });
